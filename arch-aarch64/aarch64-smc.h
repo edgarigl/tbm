d33d875f7a05e2e29d675414213822820a9f1a4e
Luc MICHEL <luc.michel@git.antfield.fr>
2017-10-04 20:08:41 +0200

    aarch64: Split aarch64-regs.h and aarch64-smc.h in their *-asm.h couterpart
    The *-asm.h files contain only #define preprocessor macros that can also
    be used in assembly files.
    
    Signed-off-by: Luc MICHEL <luc.michel@git.antfield.fr>
    Signed-off-by: Edgar E. Iglesias <edgar.iglesias@xilinx.com>

--- arch-aarch64/aarch64-smc.h
+++ arch-aarch64/aarch64-smc.h
@@ -2,22 +2,7 @@
 #include <stdint.h>
 #include <assert.h>
 
-#define SMC_ATOMIC	0x80000000
-#define SMC_A64		0x40000000
-
-#define SMC_OWNER_OEM	0x02000000
-
-#define SMC_TBM_BASE		(SMC_OWNER_OEM)
-#define SMC_TBM_NOT		(SMC_TBM_BASE | 0)
-#define SMC_TBM_RAISE_EL	(SMC_TBM_BASE | 1)
-#define SMC_TBM_MAX		(SMC_TBM_RAISE_EL)
-
-#define SMC_PSCI_BASE		0
-#define SMC_PSCI_CPU_OFF	(0x84000002)
-#define SMC_PSCI_CPU_ON		(0xc4000003)
-#define SMC_PSCI_MAX		SMC_PSCI_CPU_ON
-
-#define PSCI_SUCCESS 0
+#include "aarch64-smc-asm.h"
 
 bool plat_psci_cpu_on(unsigned int cpu, uintptr_t entry);
 bool plat_psci_cpu_off(unsigned int cpu);
