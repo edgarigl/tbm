4f290c9f3ba0268cb8e2e4e4d66d0da6ac9d1d12
Luc MICHEL <luc.michel@greensocs.com>
2018-02-22 13:06:53 +0100

    GICv2 test suite: format the debug traces for easier readability
    Signed-off-by: Luc MICHEL <luc.michel@greensocs.com>
    Signed-off-by: Edgar E. Iglesias <edgar.iglesias@xilinx.com>

--- testsuite-aarch64/gicv2/trace.h
+++ testsuite-aarch64/gicv2/trace.h
@@ -3,7 +3,12 @@
 
 #ifdef DEBUG
 # include <stdio.h>
-# define DPRINTF(fmt, ...) printf("%s: " fmt, __func__, ##__VA_ARGS__)
+# define DPRINTF(fmt, ...) do { \
+    const int PADDING = 18; \
+    int rest = PADDING - ((int)sizeof(__func__)) + 2; \
+    while (rest-- > 0) { putchar(' '); } \
+    printf("[%s] " fmt, __func__, ##__VA_ARGS__); \
+} while (0)
 #else
 # define DPRINTF(fmt, ...) do {} while(0)
 #endif
