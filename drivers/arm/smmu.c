25cf447a6a9af2713606474c82a40cf7b809d9a2
Edgar E. Iglesias <edgar.iglesias@xilinx.com>
2019-04-16 11:17:36 +0200

    arm/smmu: Update to use TBM macros
    Signed-off-by: Edgar E. Iglesias <edgar.iglesias@xilinx.com>

--- drivers/arm/smmu.c
+++ drivers/arm/smmu.c
@@ -33,15 +33,15 @@ static unsigned int dec_bussize(unsigned int v)
 
 static unsigned int cb_offset(unsigned int cb)
 {
-	return cb * (SMMU500_SMMU_CB1_SCTLR - SMMU500_SMMU_CB0_SCTLR);
+	return cb * (R_SMMU500_SMMU_CB1_SCTLR - R_SMMU500_SMMU_CB0_SCTLR);
 }
 
 bool smmu_map(bool s1, bool s2, bool write, uintptr_t va, uintptr_t *pa)
 {
 	struct smmu *m = &smmu;
 	uintptr_t regs[] = {
-		[0] = SMMU500_SMMU_GATS1PR,
-		[1] = SMMU500_SMMU_GATS12PR
+		[0] = R_SMMU500_SMMU_GATS1PR,
+		[1] = R_SMMU500_SMMU_GATS12PR
 	};
 	uint64_t addr;
 	uint64_t v = va;
@@ -56,8 +56,8 @@ bool smmu_map(bool s1, bool s2, bool write, uintptr_t va, uintptr_t *pa)
 
 	reg64_write(addr, v);
 	mb();
-	reg32_waitformask(m->base + SMMU500_SMMU_GATSR, 1, 0);
-	par = reg64_read(m->base + SMMU500_SMMU_GPAR);
+	reg32_waitformask(m->base + R_SMMU500_SMMU_GATSR, 1, 0);
+	par = reg64_read(m->base + R_SMMU500_SMMU_GPAR);
 	err = par & 1;
 	if (!err) {
 		*pa = par & ~((1ULL << 12) - 1);
@@ -81,9 +81,9 @@ void smmu_setup_tcr(struct smmu *m)
 
 		tcr = m->ctx[i]->tcr;
 		tcr |= SMMU500_SMMU_CB0_TCR_LPAE_EAE_MASK;
-		writel(m->base + cb_offset(m->cb[i]) + SMMU500_SMMU_CB0_TCR_LPAE,
+		writel(m->base + cb_offset(m->cb[i]) + R_SMMU500_SMMU_CB0_TCR_LPAE,
 				tcr);
-		writel(m->base + cb_offset(m->cb[i]) + SMMU500_SMMU_CB0_TCR2,
+		writel(m->base + cb_offset(m->cb[i]) + R_SMMU500_SMMU_CB0_TCR2,
 				tcr >> 32);
 	}
 	mb();
@@ -92,7 +92,7 @@ void smmu_setup_tcr(struct smmu *m)
 void smmu_setup_mair(struct smmu *m)
 {
 	/* MAIR is only used for stage1.  */
-	writel(m->base + cb_offset(m->cb[0]) + SMMU500_SMMU_CB0_PRRR_MAIR0,
+	writel(m->base + cb_offset(m->cb[0]) + R_SMMU500_SMMU_CB0_PRRR_MAIR0,
 		m->ctx[0]->mair);
 	mb();
 }
@@ -115,76 +115,76 @@ void smmu_init_ctx(struct mmu_ctx *s1_mmu, struct mmu_ctx *s2_mmu,
 	smmu_setup_mair(m);
 	smmu_setup_tcr(m);
 
-	writel(m->base + SMMU500_SMMU_CBA2R0, SMMU500_SMMU_CBA2R0_VA64_MASK);
+	writel(m->base + R_SMMU500_SMMU_CBA2R0, SMMU500_SMMU_CBA2R0_VA64_MASK);
 
-	r = readl(m->base + SMMU500_SMMU_SCR0);
+	r = readl(m->base + R_SMMU500_SMMU_SCR0);
 	r &= ~SMMU500_SMMU_SCR0_CLIENTPD_MASK;
-	writel(m->base + SMMU500_SMMU_SCR0, r);
+	writel(m->base + R_SMMU500_SMMU_SCR0, r);
 
-	r = readl(m->base + SMMU500_SMMU_S2CR0);
+	r = readl(m->base + R_SMMU500_SMMU_S2CR0);
 	r &= ~SMMU500_SMMU_S2CR0_TYPE_MASK;
-	writel(m->base + SMMU500_SMMU_S2CR0, r);
+	writel(m->base + R_SMMU500_SMMU_S2CR0, r);
 
-	writel(m->base + SMMU500_SMMU_SCR1, 0x00000101);
-	writel(m->base + SMMU500_SMMU_SMR0, 0xFFFE0001);
+	writel(m->base + R_SMMU500_SMMU_SCR1, 0x00000101);
+	writel(m->base + R_SMMU500_SMMU_SMR0, 0xFFFE0001);
 
 	if (enable && m->ctx[0]) {
 		phys_addr_t cb_base = m->base + cb_offset(s1_cb);
 
-		reg32_write64le(cb_base + SMMU500_SMMU_CB0_TTBR0_LOW,
+		reg32_write64le(cb_base + R_SMMU500_SMMU_CB0_TTBR0_LOW,
 				(uintptr_t) m->ctx[0]->pt.root);
 		/* No support for split yet.  */
-		reg32_write64le(cb_base + SMMU500_SMMU_CB0_TTBR1_LOW,
+		reg32_write64le(cb_base + R_SMMU500_SMMU_CB0_TTBR1_LOW,
 				(uintptr_t) m->ctx[1]->pt.root);
 
-		writel(m->base + SMMU500_SMMU_CBAR0 + s1_cb * 4,
+		writel(m->base + R_SMMU500_SMMU_CBAR0 + s1_cb * 4,
 			s2_cb << 8 |
 			3 << SMMU500_SMMU_CBAR0_TYPE_SHIFT);
 
 		mb();
 
-		r = readl(cb_base + SMMU500_SMMU_CB0_SCTLR);
+		r = readl(cb_base + R_SMMU500_SMMU_CB0_SCTLR);
 		D(printf("SMMU: SCTLR %x - >%x\n", r,
 			r | SMMU500_SMMU_CB0_SCTLR_M_MASK));
 		r |= SMMU500_SMMU_CB0_SCTLR_M_MASK;
-		writel(cb_base + SMMU500_SMMU_CB0_SCTLR, r);
+		writel(cb_base + R_SMMU500_SMMU_CB0_SCTLR, r);
 		mb();
 	}
 
 	if (enable && m->ctx[1]) {
 		phys_addr_t cb_base = m->base + cb_offset(s2_cb);
 
-		reg32_write64le(cb_base + SMMU500_SMMU_CB0_TTBR0_LOW,
+		reg32_write64le(cb_base + R_SMMU500_SMMU_CB0_TTBR0_LOW,
 				(uintptr_t) m->ctx[1]->pt.root);
 		/* No support for split yet.  */
-		reg32_write64le(cb_base + SMMU500_SMMU_CB0_TTBR1_LOW,
+		reg32_write64le(cb_base + R_SMMU500_SMMU_CB0_TTBR1_LOW,
 				(uintptr_t) m->ctx[1]->pt.root);
-		r = readl(cb_base + SMMU500_SMMU_CB0_SCTLR);
+		r = readl(cb_base + R_SMMU500_SMMU_CB0_SCTLR);
 		D(printf("SMMU: SCTLR %x - >%x\n", r,
 			r | SMMU500_SMMU_CB0_SCTLR_M_MASK));
 		r |= SMMU500_SMMU_CB0_SCTLR_M_MASK;
-		writel(cb_base + SMMU500_SMMU_CB0_SCTLR, r);
+		writel(cb_base + R_SMMU500_SMMU_CB0_SCTLR, r);
 		mb();
 	}
-	writel(m->base + SMMU500_SMMU_STLBIALL, 1);
-	writel(m->base + SMMU500_SMMU_TLBIALLNSNH, 1);
+	writel(m->base + R_SMMU500_SMMU_STLBIALL, 1);
+	writel(m->base + R_SMMU500_SMMU_TLBIALLNSNH, 1);
 	mb();
-	writel(m->base + SMMU500_SMMU_STLBGSYNC, 1);
+	writel(m->base + R_SMMU500_SMMU_STLBGSYNC, 1);
 	mb();
-	reg32_waitformask(m->base + SMMU500_SMMU_STLBGSTATUS, 1, 0);
+	reg32_waitformask(m->base + R_SMMU500_SMMU_STLBGSTATUS, 1, 0);
 }
 
 static void smmu_probe_init(struct smmu *m)
 {
 	uint32_t r;
 
-	r = readl(m->base + SMMU500_SMMU_SIDR7);
+	r = readl(m->base + R_SMMU500_SMMU_SIDR7);
 
 	printf("SMMU-500: v%d.%d at %lx %lx\n",
 		reg32_field(r, SMMU500, SMMU_SIDR7, MAJOR),
-		reg32_field(r, SMMU500, SMMU_SIDR7, MINOR), m->base, m->base + SMMU500_SMMU_SIDR0);
+		reg32_field(r, SMMU500, SMMU_SIDR7, MINOR), m->base, m->base + R_SMMU500_SMMU_SIDR0);
 
-	r = readl(m->base + SMMU500_SMMU_SIDR0);
+	r = readl(m->base + R_SMMU500_SMMU_SIDR0);
 	printf("SMMU: cttw=%d btm=%d sec=%d stages=%s.%s str-match=%d ato-ns=%d nrsidb=%d extids=%d nrsmrg=%d\n",
 		reg32_field(r, SMMU500, SMMU_SIDR0, CTTW),
 		reg32_field(r, SMMU500, SMMU_SIDR0, BTM),
@@ -198,7 +198,7 @@ static void smmu_probe_init(struct smmu *m)
 		reg32_field(r, SMMU500, SMMU_SIDR0, NUMSMRG)
 		);
 
-	r = readl(m->base + SMMU500_SMMU_SIDR1);
+	r = readl(m->base + R_SMMU500_SMMU_SIDR1);
 	printf("SMMU: pagesize=%d nrpgidx=%d nrs2cb=%d smcd=%d nrcb=%d\n",
 		reg32_field(r, SMMU500, SMMU_SIDR1, PAGESIZE),
 		reg32_field(r, SMMU500, SMMU_SIDR1, NUMPAGENDXB),
@@ -206,20 +206,20 @@ static void smmu_probe_init(struct smmu *m)
 		reg32_field(r, SMMU500, SMMU_SIDR1, SMCD),
 		reg32_field(r, SMMU500, SMMU_SIDR1, NUMCB));
 
-	r = readl(m->base + SMMU500_SMMU_SIDR2);
+	r = readl(m->base + R_SMMU500_SMMU_SIDR2);
 	printf("SMMU: r=%x 64k=%d 16k=%d 4k=%d ubs=%d oas=%d ias=%d\n", r,
 		reg32_field(r, SMMU500, SMMU_SIDR2, PTFSV8_64KB),
 		reg32_field(r, SMMU500, SMMU_SIDR2, PTFSV8_16KB),
-		reg32_field(r, SMMU500, SMMU_SIDR2, PTFSV8_4KB),
+		reg32_field(r, SMMU500, SMMU_SIDR2, TFSV8_4KB),
 		dec_bussize(reg32_field(r, SMMU500, SMMU_SIDR2, UBS)),
 		dec_bussize(reg32_field(r, SMMU500, SMMU_SIDR2, OAS)),
 		dec_bussize(reg32_field(r, SMMU500, SMMU_SIDR2, IAS))
 		);
 
 	printf("SMMU: idr[]=%x, %x, %x\n",
-			readl(m->base + SMMU500_SMMU_SIDR0),
-			readl(m->base + SMMU500_SMMU_SIDR1),
-			readl(m->base + SMMU500_SMMU_SIDR2));
+			readl(m->base + R_SMMU500_SMMU_SIDR0),
+			readl(m->base + R_SMMU500_SMMU_SIDR1),
+			readl(m->base + R_SMMU500_SMMU_SIDR2));
 }
 
 static bool smmu_probe(void *fdt, int node, const char *compat)
--- drivers/arm/smmu.h
+++ drivers/arm/smmu.h
@@ -1,11 +1,11 @@
 #include "reg-smmu.h"
 
-#define SMMU500_SMMU_GATS1PR	(0x110)
-#define SMMU500_SMMU_GATS1PW	(0x118)
-#define SMMU500_SMMU_GATS12PR	(0x130)
-#define SMMU500_SMMU_GATS12PW	(0x138)
-#define SMMU500_SMMU_GPAR	(0x180)
-#define SMMU500_SMMU_GATSR	(0x188)
+#define R_SMMU500_SMMU_GATS1PR	(0x110)
+#define R_SMMU500_SMMU_GATS1PW	(0x118)
+#define R_SMMU500_SMMU_GATS12PR	(0x130)
+#define R_SMMU500_SMMU_GATS12PW	(0x138)
+#define R_SMMU500_SMMU_GPAR	(0x180)
+#define R_SMMU500_SMMU_GATSR	(0x188)
 
 bool smmu_map(bool s1, bool s2, bool write, uintptr_t va, uintptr_t *pa);
 void smmu_init_ctx(struct mmu_ctx *s1_mmu, struct mmu_ctx *s2_mmu,
