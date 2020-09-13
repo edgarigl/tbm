c354977ed19eb9b71a9eff11e70a5d2798cb6316
Edgar E. Iglesias <edgar.iglesias@xilinx.com>
2020-09-13 16:07:46 +0200

    libminic: memmove: Call _bcopy rather than bcopy
    It seems some recent toolchains will auto-magically
    replace calls to bcopy with calls to memmove causing
    a recursive loop. Declare a local _bcopy and call
    that from memmove instead.
    
    Signed-off-by: Edgar E. Iglesias <edgar.iglesias@xilinx.com>

--- libminic/memmove.c
+++ libminic/memmove.c
@@ -1,10 +1,8 @@
 #include <string.h>
 
-#ifdef TEST_GNU_BCOPY
-void bcopy (const void *src, void *dest, size_t n)
-#else
-void bcopy (const char *src, char *dest, size_t n)
-#endif
+#undef bcopy
+
+static void _bcopy (const char *src, char *dest, size_t n)
 {
 	const unsigned char *src_u8 = (unsigned char*) src;
 	unsigned char *dst_u8 = (unsigned char*) dest;
@@ -20,8 +18,17 @@ void bcopy (const char *src, char *dest, size_t n)
 	}
 }
 
+#ifdef TEST_GNU_BCOPY
+void bcopy (const void *src, void *dest, size_t n)
+#else
+void bcopy (const char *src, char *dest, size_t n)
+#endif
+{
+	_bcopy(src, dest, n);
+}
+
 void *memmove(void *dest, const void *src, size_t n)
 {
-	bcopy(src, dest, n);
+	_bcopy(src, dest, n);
 	return dest;
 }
